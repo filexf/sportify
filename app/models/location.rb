7283581bf4a4bd4408c25913f094414dbbd3c5ef
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2024-11-26 15:04:21 +0100

    Geocode users and locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ApplicationRecord
   enum :type, [:public_park, :club_with_licence, :bar, :playground_with_booking]
-
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :playgrounds
 end
