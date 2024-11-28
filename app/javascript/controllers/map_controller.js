09604c36cd4e91d02cbeb0fabcbd5e0aa63b9009
Marion Vives <mvives.dev@gmail.com>
2024-11-28 16:26:34 +0100

    changement style de la map + des marqueurs

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     mapboxgl.accessToken = this.apiKeyValue
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10",
+      style: "mapbox://styles/mapbox/outdoors-v12",
       // center: [userValue.longitude, userValue.latitude],
       zoom: 7
     })
