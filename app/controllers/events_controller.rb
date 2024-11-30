d0886eb021d455334ac2e8621f155e54c9c62e97
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2024-11-30 19:50:29 +0100

    Change map view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -52,7 +52,7 @@ class EventsController < ApplicationController
 
     # Render all events near current user
     @near_events = Location
-      .near(current_user, 10) # le 10 devrait être variabilisé ?
+      .near(current_user, 2) # le 10 devrait être variabilisé ?
       # .flat_map { |location| location.events } idem que ligne suivante, marche pour tous les ittérateurs
       .flat_map(&:events)
 
@@ -62,6 +62,16 @@ class EventsController < ApplicationController
         info_event_html: render_to_string(partial: "info_event", locals: {event: event}),
         marker_html: render_to_string(partial: "marker", locals: {event: event})
       }}
+
+      @me_marker = {
+        name: current_user.username,
+        coordinates: {
+          lat: current_user.latitude,
+          lng: current_user.longitude },
+        marker_html: render_to_string(partial: "me_marker")
+      }
+
+      @markers = @near_event_markers + [@me_marker]
 
     @events = Event.all
     return unless params[:query] # Early return
