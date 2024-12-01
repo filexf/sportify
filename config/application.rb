a0324469eb5b78335370f04f384bcc6c0ca8df83
Félix Orain <felix.orain@gmail.com>
2024-12-01 16:18:59 +0100

    Ajout de features sur la page show

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,8 @@ module Sportify
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.assets.paths << Rails.root.join('app/assets/fonts')
+
+
   end
 end
