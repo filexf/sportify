c13f1d592f318a9a8f141f72eae59dd3e2fa980e
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2024-12-03 10:49:01 +0100

    toggle finished

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,18 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["toggle"]
   connect() {
     console.log("on est au toggler")
   }
 
   list(){
     console.log("liste");
-    document.getElementById("list").checked = true;
+    const element = document.getElementById("list").parentElement;
+    const other_element = document.getElementById("map").parentElement;
+    console.log(element);
+    element.classList.add("toggle-active");
+    other_element.classList.remove("toggle-active");
   }
 
   map(){
     console.log("carte");
-    document.getElementById("map").checked = true;
+    const element = document.getElementById("map").parentElement;
+    const other_element = document.getElementById("list").parentElement;
+    console.log(element);
+    element.classList.add("toggle-active");
+    other_element.classList.remove("toggle-active");
   }
 }
