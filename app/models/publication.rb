aa5ac208fa01a1d03d5f223f5b3d1ed5812357ce
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2024-12-05 20:06:09 +0100

    brodcast

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -7,16 +7,23 @@ class Publication < ApplicationRecord
 
   has_one_attached :photo
 
-  after_create :broadcast_publication
+  after_create_commit :broadcast_publication
 
   SPORTS = %w[Basketball Palet Padel Fléchettes Lancer\ de\ mouettes Tir\ à\ la\ mouette Tennis]
 
   private
 
   def broadcast_publication
+    if self.event.present?
     broadcast_prepend_to "publications",
-                         partial: "publications/publication",
-                         target: "publications",
-                         locals: { publication: self }
+      partial: "publications/event_creation_card",
+      target: "publications",
+      locals: { publication: self }
+    else
+    broadcast_prepend_to "publications",
+      partial: "publications/player_creation_card",
+      target: "publications",
+      locals: { publication: self }
+    end
   end
 end
